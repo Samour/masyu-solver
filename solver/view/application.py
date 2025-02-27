709547bd8fc95b818882b6aeccbaa3ae1fc812cb
Aaron Burke <aaronb@canva.com>
2025-02-27 18:51:26 +1000

    Move size selector into its own file

--- solver/view/application.py
+++ solver/view/application.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import typing
-from . import puzzle
+from . import puzzle, sizeselector
 
 
 class Application(tk.Frame):
@@ -26,59 +26,9 @@ class Application(tk.Frame):
         self._size_button.pack()
 
     def _on_changesize(self) -> None:
-        size_selector = _SizeSelector(self)
+        size_selector = sizeselector.SizeSelector(self)
         size_selector.render()
         size_selector.grab_set()
-
-
-class _SizeSelector(tk.Toplevel):
-
-    def __init__(self, master: tk.Frame):
-        super().__init__(master)
-        self._frame: typing.Optional[tk.Frame] = None
-
-    def render(self) -> None:
-        if self._frame is not None:
-            self._frame.destroy()
-
-        self._frame = tk.Frame(self)
-        warning_row = tk.Frame(self._frame)
-        warning_label = tk.Label(
-            warning_row,
-            text="Warning:",
-            fg="red",
-        )
-        warning_label.pack(side="left")
-        warning_message = tk.Label(
-            warning_row,
-            text="Changing the board size will reset all elements of the board",
-        )
-        warning_message.pack()
-        warning_row.pack()
-
-        data_row = tk.Frame(self._frame)
-        labels_column = tk.Frame(data_row)
-        height_label = tk.Label(labels_column, text="Height:")
-        height_label.pack()
-        width_label = tk.Label(labels_column, text="Width:")
-        width_label.pack()
-        labels_column.pack(side="left")
-
-        inputs_column = tk.Frame(data_row)
-        height_input = tk.Entry(inputs_column, width=2)
-        height_input.pack()
-        width_input = tk.Entry(inputs_column, width=2)
-        width_input.pack()
-        inputs_column.pack(padx=5)
-        data_row.pack()
-
-        controls_row = tk.Frame(self._frame)
-        cancel_button = tk.Button(controls_row, text="Cancel", command=self.destroy)
-        cancel_button.pack(side="left")
-        confirm_button = tk.Button(controls_row, text="Confirm")
-        confirm_button.pack()
-        controls_row.pack()
-        self._frame.pack(padx=15, pady=15)
 
 
 def main() -> None:
