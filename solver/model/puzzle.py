06ac684810a1274dbf7f36553df762f33dea7c09
Aaron Burke <aaronb@canva.com>
2025-02-28 19:52:38 +1000

    Add lines to puzzle model

--- solver/model/puzzle.py
+++ solver/model/puzzle.py
@@ -8,12 +8,20 @@ class TileType(enum.Enum):
     STRAIGHT = 3
 
 
+class LineState(enum.Enum):
+    ANY = 1
+    LINE = 2
+    EMPTY = 3
+
+
 class PuzzleState:
 
     def __init__(self, width: int, height: int):
         self._width: int = 0
         self._height: int = 0
         self._tiles: list[list[TileType]] = []
+        self._hlines: list[list[LineState]] = []
+        self._vlines: list[list[LineState]] = []
         self.reset(width, height)
 
     @property
@@ -29,6 +37,12 @@ class PuzzleState:
         self._width = width
         self._height = height
         self._tiles = [[TileType.ANY for _y in range(height)] for _x in range(width)]
+        self._hlines = [
+            [LineState.ANY for _y in range(height - 1)] for _x in range(width - 1)
+        ]
+        self._vlines = [
+            [LineState.ANY for _y in range(height - 1)] for _x in range(width - 1)
+        ]
 
     def apply(self, state: typing.Self) -> None:
         self.reset(state.width, state.height)
@@ -37,6 +51,15 @@ class PuzzleState:
                 tile = state.get_tile(x, y)
                 assert tile is not None
                 self.set_tile(x, y, tile)
+        for y in range(state.height - 1):
+            for x in range(state.width - 1):
+                hline = state.get_hline(x, y)
+                assert hline is not None
+                self.set_hline(x, y, hline)
+
+                vline = state.get_vline(x, y)
+                assert vline is not None
+                self.set_vline(x, y, vline)
 
     def get_tile(self, x: int, y: int) -> typing.Optional[TileType]:
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
@@ -47,3 +70,23 @@ class PuzzleState:
     def set_tile(self, x: int, y: int, tile: TileType) -> None:
         assert x >= 0 and x < self.width and y >= 0 and y < self.height
         self._tiles[x][y] = tile
+
+    def get_hline(self, x: int, y: int) -> typing.Optional[LineState]:
+        if x < 0 or x >= self.width - 1 or y < 0 or y >= self.height - 1:
+            return None
+
+        return self._hlines[x][y]
+
+    def set_hline(self, x: int, y: int, state: LineState) -> None:
+        assert x >= 0 and x < self.width - 1 and y >= 0 and y < self.height - 1
+        self._hlines[x][y] = state
+
+    def get_vline(self, x: int, y: int) -> typing.Optional[LineState]:
+        if x < 0 or x >= self.width - 1 or y < 0 or y >= self.height - 1:
+            return None
+
+        return self._vlines[x][y]
+
+    def set_vline(self, x: int, y: int, state: LineState) -> None:
+        assert x >= 0 and x < self.width - 1 and y >= 0 and y < self.height - 1
+        self._vlines[x][y] = state
