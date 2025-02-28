14e22a20a09fedd7844009df3fc464cc7a0316e0
Aaron Burke <aaronb@canva.com>
2025-02-28 11:56:18 +1000

    Start work on confirm button
    Enable/disable it based on input values

--- solver/view/sizeselector.py
+++ solver/view/sizeselector.py
@@ -8,8 +8,11 @@ class SizeSelector(tk.Toplevel):
     def __init__(self, master: tk.Frame):
         super().__init__(master)
         self._frame: typing.Optional[tk.Frame] = None
+        self._confirm_button: typing.Optional[tk.Button] = None
 
-        self._validate_command = self.register(self._on_validate), "%P"
+        self._width_value: typing.Optional[int] = None
+        self._height_value: typing.Optional[int] = None
+        self._validate_command = self.register(self._on_validate)
 
     def render(self) -> None:
         if self._frame is not None:
@@ -43,11 +46,15 @@ class SizeSelector(tk.Toplevel):
             inputs_column,
             width=2,
             validate="key",
-            validatecommand=self._validate_command,
+            validatecommand=(self._validate_command, "h", "%P"),
         )
+        height_input.bind()
         height_input.pack()
         width_input = tk.Entry(
-            inputs_column, width=2, validatecommand=self._validate_command
+            inputs_column,
+            width=2,
+            validate="key",
+            validatecommand=(self._validate_command, "w", "%P"),
         )
         width_input.pack()
         inputs_column.pack(padx=5)
@@ -56,10 +63,44 @@ class SizeSelector(tk.Toplevel):
         controls_row = tk.Frame(self._frame)
         cancel_button = tk.Button(controls_row, text="Cancel", command=self.destroy)
         cancel_button.pack(side="left")
-        confirm_button = tk.Button(controls_row, text="Confirm")
-        confirm_button.pack()
+        self._confirm_button = tk.Button(
+            controls_row, text="Confirm", state="disabled", command=self._on_confirm
+        )
+        self._confirm_button.pack()
         controls_row.pack()
         self._frame.pack(padx=15, pady=15)
 
-    def _on_validate(self, v: str) -> bool:
-        return tk.TRUE if re.match(r"^\d{0,2}$", v) is not None else tk.FALSE
+    def _on_validate(self, field: typing.Literal["h", "w"], v: str) -> bool:
+        if re.match(r"^\d{0,2}$", v) is None:
+            return tk.FALSE
+
+        if len(v) == 0:
+            self._update_field(field, None)
+            return tk.TRUE
+
+        value = int(v)
+        if value > 0:
+            self._update_field(field, value)
+        else:
+            self._update_field(field, None)
+        return tk.TRUE
+
+    def _update_field(
+        self, field: typing.Literal["h", "w"], value: typing.Optional[int]
+    ) -> None:
+        if field == "h":
+            self._height_value = value
+        elif field == "w":
+            self._width_value = value
+
+        assert self._confirm_button is not None
+        self._confirm_button.configure(
+            state=(
+                "normal"
+                if self._height_value is not None and self._width_value is not None
+                else "disabled"
+            )
+        )
+
+    def _on_confirm(self) -> None:
+        print(f"Confirming size ({self._width_value},{self._height_value})")
