0b8b22eb256ba4c7bb2fbf5a9b6496608ab6fc56
Aaron Burke <aaronb@canva.com>
2025-02-28 17:08:23 +1000

    Add buttons for load and save

--- solver/view/application.py
+++ solver/view/application.py
@@ -11,12 +11,15 @@ class Application(tk.Frame):
         self._puzzle_state = puzzle_state
         self._puzzle_view: typing.Optional[puzzle.PuzzleView] = None
         self._change_size_button: typing.Optional[_ChangeSizeButton] = None
+        self._save_load_controls: typing.Optional[_SaveLoadControls] = None
 
     def render(self) -> None:
         if self._puzzle_view is not None:
             self._puzzle_view.destroy()
         if self._change_size_button is not None:
             self._change_size_button.destroy()
+        if self._save_load_controls is not None:
+            self._save_load_controls.destroy()
 
         self.pack(padx=50, pady=30)
         self._puzzle_view = puzzle.PuzzleView(self, self._puzzle_state)
@@ -28,6 +31,8 @@ class Application(tk.Frame):
             rerender_puzzle=self._puzzle_view.render,
         )
         self._change_size_button.render()
+        self._save_load_controls = _SaveLoadControls(self)
+        self._save_load_controls.render()
 
 
 class _ChangeSizeButton(tk.Frame):
@@ -47,7 +52,7 @@ class _ChangeSizeButton(tk.Frame):
             self, text="Change size", command=self._on_changesize
         )
         self._size_button.pack()
-        self.pack()
+        self.pack(pady=5)
 
     def _on_changesize(self) -> None:
         size_selector = sizeselector.SizeSelector(
@@ -59,6 +64,26 @@ class _ChangeSizeButton(tk.Frame):
     def _on_changesize_confirm(self, width: int, height: int) -> None:
         self._puzzle_state.reset(width, height)
         self._rerender_puzzle()
+
+
+class _SaveLoadControls(tk.Frame):
+
+    def __init__(self, master: tk.Frame):
+        super().__init__(master)
+        self._load_button: typing.Optional[tk.Button] = None
+        self._save_button: typing.Optional[tk.Button] = None
+
+    def render(self) -> None:
+        if self._load_button is not None:
+            self._load_button.destroy()
+        if self._save_button is not None:
+            self._save_button.destroy()
+
+        self._load_button = tk.Button(self, text="Load puzzle")
+        self._load_button.pack(side="left", padx=5)
+        self._save_button = tk.Button(self, text="Save puzzle")
+        self._save_button.pack(padx=5)
+        self.pack(pady=5)
 
 
 def main(puzzle_state: model.PuzzleState) -> None:
