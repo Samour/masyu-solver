d171c231504c6a00c6e342e8c90fde76d387b8eb
Aaron Burke <aaronb@canva.com>
2025-03-01 21:32:15 +1000

    Add a delay to the solving thread

--- solver/view/solvethread.py
+++ solver/view/solvethread.py
@@ -1,5 +1,9 @@
+import time
 import threading
 from solver import algorithm, messaging, model
+
+
+_DELAY = 0.5
 
 
 def start_solver(
@@ -12,30 +16,42 @@ def start_solver(
     thread.start()
 
 
+class _PublishingPuzzle(model.PuzzleState):
+
+    def __init__(self, width: int, height: int, publisher: messaging.Publisher) -> None:
+        super().__init__(width, height)
+        self.publishing: bool = False
+        self._publisher = publisher
+
+    def set_hline(self, x: int, y: int, state: model.LineState) -> None:
+        super().set_hline(x, y, state)
+
+        if self.publishing:
+            self._publisher.send(messaging.UpdateHLine(x=x, y=y, state=state))
+            self._delay()
+
+    def set_vline(self, x: int, y: int, state: model.LineState) -> None:
+        super().set_vline(x, y, state)
+
+        if self.publishing:
+            self._publisher.send(messaging.UpdateVLine(x=x, y=y, state=state))
+            self._delay()
+
+    def _delay(self) -> None:
+        if _DELAY > 0:
+            time.sleep(_DELAY)
+
+
 class _SolverThread(threading.Thread):
 
-    def __init__(self, puzzle_state: model.PuzzleState, publisher: messaging.Publisher):
+    def __init__(self, puzzle_state: _PublishingPuzzle, publisher: messaging.Publisher):
         super().__init__()
         self._puzzle = puzzle_state
         self._publisher = publisher
 
     def run(self) -> None:
+        self._puzzle.publishing = True
         solver = algorithm.Solver(puzzle_state=self._puzzle)
         solver.solve()
 
         self._publisher.send(messaging.SolverCompleted())
-
-
-class _PublishingPuzzle(model.PuzzleState):
-
-    def __init__(self, width: int, height: int, publisher: messaging.Publisher) -> None:
-        super().__init__(width, height)
-        self._publisher = publisher
-
-    def set_hline(self, x: int, y: int, state: model.LineState) -> None:
-        super().set_hline(x, y, state)
-        self._publisher.send(messaging.UpdateHLine(x=x, y=y, state=state))
-
-    def set_vline(self, x: int, y: int, state: model.LineState) -> None:
-        super().set_vline(x, y, state)
-        self._publisher.send(messaging.UpdateVLine(x=x, y=y, state=state))
